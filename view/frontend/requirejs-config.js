5bbe8ea24dd476f276502e5e02eb044370065d3e
John Hughes <johnh@fisheyehq.com>
2018-02-15 22:54:54 +0000

    Add lazy sizes script via require JS

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+/**
+ * Copyright © Fisheye Media Ltd. All rights reserved.
+ * See LICENCE.txt for licence details.
+ */
+var config = {
+	deps: [
+        'Fisheye_Lazyload/js/lazysizes.min'
+	]
+};
