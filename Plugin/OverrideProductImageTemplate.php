63273841c57c55b83b15a8dddbcb93f5264bd0a4
Rob Aimes <roba@fisheyehq.com>
2018-02-20 13:34:20 +0000

    Code review fixes

--- Plugin/OverrideProductImageTemplate.php
+++ Plugin/OverrideProductImageTemplate.php
@@ -3,20 +3,22 @@
  * Copyright © Fisheye Media Ltd. All rights reserved.
  * See LICENCE.txt for licence details.
  */
-namespace Fisheye\Lazyload\Plugin\Block\Product;
+namespace Fisheye\Lazyload\Plugin;
 
+use Magento\Catalog\Block\Product\Image as ImageSubject;
 
-class Image
+class OverrideProductImageTemplate
 {
 
     /**
      * Plugin for lazyloading images - Set template to Fisheye_Lazyload
      *
-     * @param \Magento\Catalog\Block\Product\Image $subject
+     * @param ImageSubject $subject
      * @param $template
      * @return array
      */
-    public function beforeSetTemplate(\Magento\Catalog\Block\Product\Image $subject, $template) {
+    public function beforeSetTemplate(ImageSubject $subject, $template)
+    {
         $template = str_replace('Magento_Catalog', 'Fisheye_Lazyload', $template);
         return [$template];
     }
