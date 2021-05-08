6bbd2d52a6dfd839ad570c4756dd7c936a3fa3e6
Dan Wallis <danw@fisheyehq.com>
2021-05-08 22:58:42 +0100

    Work around bug in Magento's mixins.js
    The `processNames` method doesn't cope with a null name, which is what happens
    when a module correctly registers itself anonymously, using the Univeral Module
    Definition pattern. This is needed as lazysizes plugins rely on RequireJS
    working properly.
    
    References:
    https://github.com/magento/magento2/blob/2.4.2/lib/web/mage/requirejs/mixins.js#L191-L193
    https://github.com/umdjs/umd/blob/v1.0.0/returnExportsGlobal.js#L19

--- view/base/requirejs-config.js
+++ view/base/requirejs-config.js
@@ -9,6 +9,18 @@ var config = {
         }
     },
     deps: [
-        'lazysizes'
+        'lazysizes',
     ],
+    shim: {
+        'Fisheye_Lazyload/js/lazysizes.min': {
+            // Yes, there is a RequireJS-compatible (UMD) version of lazysizes
+            // available and this works just fine on its own. However there's a
+            // bug in Magento's mixins.js that prevents anything using the UMD
+            // pattern from working at all. Instead, we're using the 'global'
+            // version of lazysizes and this 'shim' configuration to map it into
+            // RequireJS. We need to have RequireJS working properly for
+            // lazysizes plugins to work well.
+            exports: 'lazySizes',
+        },
+    },
 };
